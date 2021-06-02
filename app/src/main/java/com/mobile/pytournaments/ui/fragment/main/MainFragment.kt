3e4250f61a5d7845d145eb56c96c3e3d9fc2387f
ViniciusJMR <viniciusdejesus605@gmail.com>
2021-06-02 08:54:51 -0300

    Consertado binding no Main Fragment

--- app/src/main/java/com/mobile/pytournaments/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/mobile/pytournaments/ui/fragment/main/MainFragment.kt
@@ -21,8 +21,16 @@ class MainFragment : Fragment() {
 
     private val viewModel: TournamentViewModel by viewModels()
 
+    fun showTestBallon(v : View){
+        Toast.makeText(requireContext(), "teste", Toast.LENGTH_LONG).show()
+    }
+
+    fun goToTournamentScreen(v : View){
+        findNavController().navigate(R.id.action_mainFragment_to_tournamentsDescriptionFragment)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
 
         binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -40,22 +48,23 @@ class MainFragment : Fragment() {
         try {
 
             viewModel.subscribedTournaments.observe(viewLifecycleOwner) { list ->
-                binding.rvTorneiosParticipados.adapter = Grid_RecyclerView(list)
+                binding.titulo.text = list[0].name
+                binding.descricaoJogo.text = list[0].description
+                binding.TournamentDate.text = list[0].date.toString()
+
+                binding.rvTorneiosParticipados.adapter = Grid_RecyclerView(list.slice(1 until list.size))
             }
             loadSubscribedTournaments()
         }
         catch(e : Exception){
-            Toast.makeText(context, "Ocorreu algum erro!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Ocorreu algum erro ao carregar os torneios!!", Toast.LENGTH_SHORT).show()
             println(e.printStackTrace())
         }
-    }
-
-    fun goToTournamentScreen(){
-        findNavController().navigate(R.id.action_mainFragment_to_tournamentsDescriptionFragment)
     }
 
     fun loadSubscribedTournaments(){
         viewModel.loadSubscribedTournaments()
     }
+
 
 }
--- app/src/main/java/com/mobile/pytournaments/ui/fragment/main/tournaments/MainTournamentsFragment.kt
+++ app/src/main/java/com/mobile/pytournaments/ui/fragment/main/tournaments/MainTournamentsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -49,4 +50,6 @@ class MainTournamentsFragment : Fragment() {
     private fun loadTournamentDataOnScreen(){
         viewModel.loadSearchedTournaments()
     }
+
+
 }
